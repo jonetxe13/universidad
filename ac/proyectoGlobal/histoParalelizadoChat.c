02f036d5ef23c1436e0e813d9db7c44fe8c0779e
jonetxe13 <jonetxeba.je10@gmail.com>
2023-04-28 08:02:27 +0200

    version paralela con chatgpt ns si ira

---
+++ ac/proyectoGlobal/histoParalelizadoChat.c
@@ -0,0 +1,73 @@
+/** PAR, Procesadores de Alto Rendimiento - GII (Ingenieria de Computadores)
+ histo.c -- versión serie
+ - paralelización de bucles
+ - tipos de variables
+ - paralelismo de grano fino y de grano grueso
+ - reparto de iteraciones (scheduling)
+***************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#define N 5000
+#define MAX 10 
+
+int mat[N][N];
+
+int main (){
+ int i, j, k, histo[MAX], hmin, imin;
+ int suma_fil[N], suma_col[N];
+ // valores iniciales, NO paralelizar
+ for(i=0; i<N; i++)
+ for(j=0; j<N; j++) {
+   if (i%3) mat[i][j] = (i+j) % MAX;
+   else mat[i][j] = (i+i*j) % MAX;
+ }
+
+ for (i=0; i<MAX; i++) histo[i] = 0;
+ for (i=0; i<N; i++){
+   suma_fil[i] = 0;
+   suma_col[i] = 0;
+ }
+
+// ========================
+// === PARA PARALELIZAR ===
+// ========================
+#pragma omp parallel
+{
+    #pragma omp for
+    for (int i=0; i<N; i++)
+        for (int j=0; j<N; j++)
+            #pragma omp atomic
+            histo[mat[i][j]]++; // histograma
+
+    #pragma omp for
+    for (int i=0; i<N; i++) // suma de las filas de la matriz
+        for (int j=0; j<N; j++)
+            suma_fil[i] += mat[i][j];
+
+    #pragma omp for
+    for (int j=0; j<N; j++) // suma de las columnas de la matriz
+        for (int i=0; i<N; i++)
+            suma_col[j] += mat[i][j];
+}
+
+hmin = N*N + 1;
+for (int i=0; i<MAX; i++){ // el valor minimo de la histograma y su posicion
+    if (hmin > histo[i]){
+        hmin = histo[i];
+        imin = i;
+    }
+}
+
+// === FIN DE LA REGION PARALELA ===
+  
+ printf ("\n Histograma de la matriz mat[%d][%d] (con %d valores diferentes)\n", N, N, MAX);
+ printf (" ===========================================================\n");
+ for (k=0; k<MAX; k++) printf ("%9d", k);
+ printf ("\n");
+ for (k=0; k<MAX; k++) printf ("%9d", histo[k]);
+ printf ("\n");
+ printf ("\n El minimo de la histograma: %d, posicion %d\n", hmin, imin);
+ printf ("\n La suma de la fila 100: %d ", suma_fil[100]);
+ printf ("\n La suma de la columna 200: %d \n\n", suma_col[200]);
+
+}
---
+++ ac/proyectoGlobal/mcolParalelizadoChat.c
@@ -0,0 +1,110 @@
+/** PAR, Procesadores de Alto Rendimiento - GII (Ingenieria de Computadores)
+ mcol.c -- versión serie
+ - paralelización de bucles
+ - tipos de variables
+ - paralelismo de grano fino y de grano grueso
+ - reparto de iteraciones (scheduling)
+***************************************************************************/
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#define NFIL 200000
+#define NCOL 300
+#define MAXVAL 60
+double mat[NFIL][NCOL];
+
+// valores iniciales de la matriz (al azar); no paralelizar
+static void inicializar_mat (double mat[NFIL][NCOL]){
+
+ int i, j;
+ for (i=0; i<NFIL; i++)
+   for (j=0; j<NCOL; j++)
+    mat[i][j] = rand () % MAXVAL - (j % 59);
+
+}
+// lectura del fichero de entrada; devuelve el número de peticiones
+int leer_peticiones (char *fpet, int *columnas){
+
+ FILE *fich;
+ int i, x;
+
+ fich = fopen (fpet, "r");
+ i = x = 0;
+
+ while (x != EOF) {
+  x = fscanf (fich, "%d", columnas + i);
+  i++;
+ }
+  
+ fclose (fich);
+ return (i-1);
+
+}
+// escritura del fichero de salida: resultados
+void escribir_resultados (char *fsol, int npet, double *resultados){
+
+ FILE *fich;
+ int i;
+ fich = fopen (fsol, "w");
+
+ for (i=0; i<npet; i++)
+ fprintf (fich, "%10.3f\n", resultados[i]);
+ fclose (fich);
+ return;
+
+}
+// "calculo": procesar las columnas de la matriz
+// ========================
+// === PARA PARALELIZAR ===
+// ========================
+void procesar_peticiones (double mat[NFIL][NCOL], int *columnas, int cant, 
+
+ double *resultados){
+ int i, j, columna;
+ double y, suma;
+ #pragma omp parallel for private(i, columna, y, suma)
+ for (j=0; j<cant; j++){
+   columna = columnas[j];
+   suma = 0.0;
+
+   for (i=0; i<NFIL; i++){
+    y = mat[i][columna];
+    if (y > 0) suma += exp (y / 100); 
+   }
+
+   resultados[j] = suma;
+
+ }
+}
+
+// programa principal
+int main (int argc, char *argv[]){
+
+ int num_pet, columnas[NCOL];
+ double *resultados, Tej;
+ struct timespec t1, t0;
+ char fsol[20];
+
+ if (argc != 2){
+   printf ("\n Ejecutar: %s f1\n\n", argv[0]);
+   return 0;
+ }
+
+ inicializar_mat (mat);
+ num_pet = leer_peticiones (argv[1], columnas);
+ resultados = malloc (num_pet * sizeof (double));
+ clock_gettime (CLOCK_REALTIME, &t0);
+ procesar_peticiones (mat, columnas, num_pet, resultados);
+ clock_gettime (CLOCK_REALTIME, &t1);
+ Tej = (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec) / (double)1e9;
+ 
+ strcpy (fsol, argv[1]);
+ strcat (fsol, "_sol"); // la version paralela: _sol_p
+ escribir_resultados (fsol, num_pet, resultados);
+ printf ("\n Peticiones: %d -- Ultimo resul.: %1.3f\n", num_pet, resultados[num_pet - 1]);
+ printf (" Tej: %1.3f ms\n\n", Tej*1000);
+ return (0);
+
+}
