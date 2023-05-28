25afb1a6139227bd538889d4ea9015aa11c25bd5
jonetxe13 <jonetxeba.je10@gmail.com>
2023-05-28 13:53:14 +0200

    retoque

--- ISO/miProyectoFinal/util2/util2.c
+++ ISO/miProyectoFinal/util2/util2.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
   DIR *dir;
   struct dirent *rdir;
   if(argc != 2){
-    printf("Uso: %s directorio");
+    printf("Uso: %s directorio", argv[0]);
   }
   if((dir=opendir(argv[1])) == NULL){
     printf("error abriendo el directorio");
@@ -29,23 +29,6 @@ int main(int argc, char *argv[])
     strcat(filepath, "/");
     strcat(filepath, rdir->d_name);
     if(strcmp(rdir->d_name, ".") != 0 && strcmp(rdir->d_name, "..") != 0){
-      // if (rdir->d_type == DT_REG) {
-      //   printf("nombre: %s  mode: %u\n", rdir->d_name , fst.st_mode);
-      //   //eliminar los permisos de grupo
-      //   new_mode = 0100600;
-      //   printf("nuevo mode: %u\n", new_mode);
-      // }
-      // else if (rdir->d_type == DT_DIR) {
-      //   printf("nombre: %s  mode: %u\n", rdir->d_name, fst.st_mode);
-      //   new_mode = 0040770;
-      // }
-      // else{
-      //   printf("nombre: %s  mode: %u\n", rdir->d_name, fst.st_mode);
-      //   new_mode = 0040600;
-      // }
-      // if (chmod(filepath, new_mode) == -1) {
-      //   write(1, "Error changing permissions\n",strlen("Error changing permissions\n"));
-      // }
         if (rdir->d_type == DT_REG) {
         printf("nombre: %s  mode: %u\n", rdir->d_name , fst.st_mode);
         //eliminar los permisos de grupo y others
@@ -66,7 +49,6 @@ int main(int argc, char *argv[])
           write(1, "Error changing permissions\n",strlen("Error changing permissions\n"));
       }
 
-      printf("ha funcionado?");
       close(f);
       return 0;
     }
