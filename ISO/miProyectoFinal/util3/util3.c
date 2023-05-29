2ef0741ca661ac09b17710200dcdb67f871b00cc
jonetxe13 <jonetxeba.je10@gmail.com>
2023-05-29 12:18:55 +0200

    util3 reloj annadido

--- ISO/miProyectoFinal/util3/util3.c
+++ ISO/miProyectoFinal/util3/util3.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]){
-  if(argc != 4){
+  clock_t start = clock();
+  if(argc <= 3){
     printf("Uso: %s programa1 programa2", argv[0]);
   }
-  // printf("hola que tal\n");
   if(argv[1][0] == 'C'){
     // printf("se mete en el primer if\n");
-    if(fork() == 0){
-      char *pr[] = { argv[2], NULL };
-      execvp(argv[2], pr);
-      printf("error en execlp1\n");
-      _exit(-1);
-    }
-    if(fork() == 0){
-      char *pr2[] = { argv[3], NULL };
-      execvp(argv[3], pr2);
-      printf("error en execlp2\n");
-      _exit(-1);
+    for(int i = 0; i < argc-1; i++){
+      if(fork() == 0){
+        printf("%s\n", argv[i+2]);
+        char *pr[] = { argv[i+2], NULL };
+        execvp(argv[i+2], pr);
+        printf("error en execlp1\n");
+        _exit(-1);
+      }
     }
     wait(NULL);
   }
-  if( argv[1][0] == 'S'){
-    for(int i = 0; i < 2; i++){
+  if(argv[1][0] == 'S'){
+    for(int i = 0; i < argc-1; i++){
     // printf("se mete en el for\n");
       if(fork() == 0){
+        printf("%s\n", argv[i+2]);
         char *pr3[] = { argv[i+2], NULL };
         execvp(argv[i+2], pr3);
         printf("error en execlp2\n");
@@ -35,5 +34,8 @@ int main(int argc, char *argv[]){
       wait(NULL);
     }
   }
+  clock_t end = clock();
+  float tiempo = (float)(end - start) / 1000;
+  printf("(el tiempo total es: %f)", tiempo);
   return 1;
 }
